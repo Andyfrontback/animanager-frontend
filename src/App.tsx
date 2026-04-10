44db3441fb6b301ee8787cc4a4874966e6968803
Andy AM <edinsonandresarizamendoza@gmail.com>
2026-04-09 21:50:49 -0500

    feat(seo): Improves index.html meta tags definition

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,18 @@ import { ThemeProvider } from "./components/ui/theme-provider";
 import ErrorBoundary from "./ErrorBoundary";
 import { queryClient } from "./utils";
 import { HelmetProvider } from "react-helmet-async";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    // Buscamos todas las etiquetas que tienen la clase .static-seo
+    const staticElements = document.querySelectorAll(".static-seo");
+
+    // Las eliminamos del DOM de forma segura
+    staticElements.forEach((el) => el.remove());
+
+    // SEO Estático eliminado. React Helmet toma el control.
+  }, []);
   /* Aquí orquestamos llamados a providers de contextos globales definidos por nosotros */
   return (
     <ErrorBoundary>
